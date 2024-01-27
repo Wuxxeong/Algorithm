1ff5f3d914884512c8ced90876e62dd8c32c309d
WooSung Shin <sws326666@gmail.com>
2024-01-27 17:53:30 +0900

    [level 3] Title: 네트워크, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,7 +1,6 @@
 from collections import deque
 def solution(n, computers):
     graph = 0
-    parent = [-1 for _ in range(n)]
     visited = [False for _ in range(n)]
     queue = deque()
     
@@ -10,11 +9,11 @@ def solution(n, computers):
             queue.append(start_node)
             visited[start_node] = True
             graph += 1
+
         while queue:
             row = queue.popleft()
             for col in range(0, n):
                 if computers[row][col] == 1  and not visited[col]:
-                    parent[col] = row
                     queue.append(col)
                     visited[col] = True
     
