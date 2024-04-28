e8f33a15fe0c691891700cf7375848e6f0ef96b5
WooSung Shin <sws326666@gmail.com>
2024-04-28 23:56:27 +0900

    [level 3] Title: 가장 먼 노드, Time: 22.67 ms, Memory: 19.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+def solution(n, edge):
+    answer = 0
+    graph = [[] for _ in range(n+1)]
+    visited = [0] * (n + 1)
+    
+    for eg in edge:
+        a, b = eg
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    q = deque()
+    q.append(1)
+    visited[1] = 1
+    
+    while q:
+        x = q.popleft()
+        for i in graph[x]:
+            if not visited[i]:
+                visited[i] = visited[x]+1
+                q.append(i)
+    
+    max_value = max(visited)
+    answer = visited.count(max_value)
+            
+    return answer
