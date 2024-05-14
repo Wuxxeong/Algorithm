e90c24ec5d5d4abf540f91a5caeefb236d1e13d6
WooSung Shin <sws326666@gmail.com>
2024-05-14 22:45:51 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,6 +1,6 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 15일 17:10:41
+2024년 05월 14일 22:45:48
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,18 +1,24 @@
+import heapq
+
 def solution(jobs):
     answer = 0
-    total = 0
-    finish = 0
+    now = 0 #현재시간
+    i = 0   #처리개수
+    start = -1 #마지막 완료시간
+    heap = []
     
-    jobs.sort(key = lambda x:x[1]-x[0])
-    
-    for job in jobs:
-        waiting = finish-job[0]
-        if waiting > 0:
-            total = total+waiting+job[1]
-            finish += job[1]
+    while i < len(jobs):
+        for job in jobs:
+            if start < job[0] <= now:
+                heapq.heappush(heap,[job[1],job[0]])
+        
+        if heap:
+            current = heapq.heappop(heap)
+            start = now
+            now += current[0]
+            answer += now - current[1] 
+            i += 1
         else:
-            total += job[1]
-            finish += job[1]
-    
-    answer = total//len(jobs)
-    return answer
+            now += 1
+            
+    return answer // len(jobs)
