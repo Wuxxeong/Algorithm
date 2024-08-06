faaa1ba78886e77f6e07ebc8f4086700aa1d43d1
WooSung Shin <sws326666@gmail.com>
2024-08-06 11:38:10 +0900

    [Silver V] Title: 폴리오미노, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,32 +1,22 @@
-p = input()
-result = ''
-length = 0
-for i in range(len(p)):
-    if p[i]=='X':
-        length+=1
-        continue
-    elif p[i]=='.':
-        if length%2!=0:
-            result = -1
-            break
-        while length>0:
-            if length>=4:
-                result += 'AAAA'
-                length -= 4
-            else:
-                result += 'BB'
-                length -= 2
-        result += '.'
-if length>0:
-    if length%2!=0:
-        result = -1
-    else:
-        while length>0:
-            if length>=4:
-                result += 'AAAA'
-                length -= 4
-            else:
-                result += 'BB'
-                length -= 2
+def convert_polymino(p):
+    result = ''
+    length = 0
+    
+    for char in p:
+        if char == 'X':
+            length += 1
+        elif char == '.':
+            if length%2 != 0:
+                return -1
+            result += 'AAAA' * (length // 4) + 'BB' * ((length%4)//2)
+            result += '.'
+            length = 0
+            
+    if length > 0:
+        if length %2 != 0:
+            return -1
+        result += 'AAAA' * (length // 4) + 'BB' * ((length%4)//2)
+    return result
 
-print(result)
+p = input()
+print(convert_polymino(p))
