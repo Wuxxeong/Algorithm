9b5ab181bce09da515265b2be78afe2d1c17ca85
WooSung Shin <sws326666@gmail.com>
2024-08-12 14:32:57 +0900

    [Gold V] Title: 치킨 배달, Time: 176 ms, Memory: 111428 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -0,0 +1,36 @@
+import math
+from itertools import combinations
+
+
+def calculateDistance(house, chicken):
+    min_d = 0
+    for h in house:
+        min_h = float(math.inf)
+        for ch in chicken:
+            ch_dist = abs(h[0] - ch[0]) + abs(h[1] - ch[1])
+            if ch_dist < min_h:
+                min_h = ch_dist
+        min_d += min_h
+    return min_d
+
+
+N, M = map(int, input().split())
+cities = [[0] * (N + 1)]
+for _ in range(N):
+    cities.append([0] + list(map(int, input().split())))
+
+chicken = []
+house = []
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        if cities[i][j] == 1:
+            house.append([i, j])
+        elif cities[i][j] == 2:
+            chicken.append([i, j])
+
+distance = float(math.inf)
+for ch in list(combinations(chicken, M)):
+    cd = calculateDistance(house, ch)
+    if cd < distance:
+        distance = cd
+print(distance)
