43b3170dd1fb7168b8970dbf6b267eb994172471
WooSung Shin <sws326666@gmail.com>
2024-10-16 11:00:29 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 87.78 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,23 +1,28 @@
+from collections import deque
 def solution(n, wires):
-    answer = []
+    answer = n
+    G = [[0]*n for _ in range(n)]
+    for a,b in wires:
+        G[a-1][b-1] = 1
+        G[b-1][a-1] = 1
     
-    for wire in wires: #O(n)
-        G1, G2 = [wire[0]], [wire[1]]
-        wire_list = wires.copy()
-        wire_list.remove(wire) #O(n)
+    for ra,rb in wires:
+        #rb를 기준으로 탐색
+        q = deque()
+        visited = [0]*n
         
-        while wire_list:
-            sub = wire_list.pop(0)
-            if sub[0] in G1:
-                G1.append(sub[1])
-            elif sub[1] in G1:
-                G1.append(sub[0])
-            elif sub[0] in G2:
-                G2.append(sub[1])
-            elif sub[1] in G2:
-                G2.append(sub[0])
-            else:
-                wire_list.append(sub)
-    
-        answer.append(abs(len(list(set(G1)))-len(list(set(G2)))))
-    return min(answer)
+        q.append(rb)
+        visited[rb-1]=1
+        cnt = 1
+        while q:
+            cur = q.popleft()
+            for k in range(n):
+                if k==ra: continue
+                if (G[cur-1][k-1]) and not visited[k-1]:
+                    q.append(k)
+                    visited[k-1]=1
+                    cnt+=1
+        
+        if abs(n-cnt-cnt)<answer:
+            answer = abs(n-cnt-cnt)
+    return answer
