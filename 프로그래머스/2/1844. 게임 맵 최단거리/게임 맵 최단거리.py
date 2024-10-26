e601ef0c603563ca2a9b7e1888e3f73de8019a96
WooSung Shin <sws326666@gmail.com>
2024-10-26 15:52:09 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 16.59 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,24 +1,26 @@
 from collections import deque
+
 def solution(maps):
-    answer = 0
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-
-    queue = deque()
-    queue.append((0,0))		# 0,0에서 시작
-
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            N = len(maps)
-            M = len(maps[0])
-            if 0<=nx<N and 0<=ny<M and maps[nx][ny]==1:
-                maps[nx][ny] = maps[x][y]+1
-                queue.append((nx,ny))
-    answer = maps[N-1][M-1]
+    row,col=len(maps), len(maps[0])
+    mn_d=row*col
+    si,sj=0,0
+    ei,ej=row-1,col-1
+    answer=[-1]
+    q=deque()
+    q.append((si,sj,1))
+    visited=set()
+    visited.add((si,sj))
+    while q:
+        ci,cj,d=q.popleft()
+        if (ci,cj)==(ei,ej):
+            answer.append(d)
+        for di,dj in ((0,1),(1,0),(-1,0),(0,-1)):
+            ni,nj=ci+di,cj+dj                
+            if 0<=ni<row and 0<=nj<col and maps[ni][nj]==1 and (ni,nj) not in visited:
+                q.append((ni,nj,d+1))
+                visited.add((ni,nj))
     
-    if answer ==1:
-        answer = -1
-    return answer
+    if len(answer)==1:
+        return -1
+    answer.sort()
+    return answer[1]
