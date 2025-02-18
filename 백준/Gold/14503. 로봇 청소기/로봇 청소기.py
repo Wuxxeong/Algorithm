fe0a45bf92399b7eba5499bc3b95eabea7979296
WooSung Shin <sws326666@gmail.com>
2025-02-18 21:48:57 +0900

    [Gold V] Title: 로봇 청소기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,37 +1,47 @@
-N,M = map(int, input().split())
-r, c, d = map(int, input().split())
-section = [list(map(int, input().split())) for _ in range(N)]
-cleaned = [[0]*M for _ in range(N)]
-dr, dc = [-1,0,1,0],[0,1,0,-1]
+'''
+status = 0 -> 청소되지 않은 빈 칸
+         1 -> 벽
+
+로봇 청소기가 있는 칸 == 빈 칸
+
+청소하는 영역의 개수
+'''
+
+N,M = map(int,input().split())
+r,c,d = map(int,input().split()) #초기 (r,c) 방향 d 방향
+status = [list(map(int,input().split())) for _ in range(N)]
+
+dr,dc = [-1,0,1,0],[0,1,0,-1] #북동남서
+
+cnt = 0
+
+visited = set()
+def isCleaned(r, c):
+    for i in range(4):
+        nr,nc = r+dr[i],c+dc[i]
+        if 0<=nr<N and 0<=nc<M and (nr,nc) not in visited and status[nr][nc] == 0: #청소되지 않은 빈칸
+            return True
+    return False
 
 canMove = True
-cnt = 0
 while canMove:
-    if cleaned[r][c] == 0:
-        cleaned[r][c] = 1
+    # [1] 현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다.
+    if (r,c) not in visited and status[r][c] == 0:
+        # status[r][c] = 1
+        visited.add((r,c))
         cnt += 1
-    notCleaned = False
-    for i in range(4):
-        nr, nc = r+dr[i], c+dc[i]
-        if section[nr][nc] == 0 and cleaned[nr][nc] == 0:
-            notCleaned = True
-            break
-
-    if notCleaned:
-        d = (d+3)%4 #반시계 90도 회전
-        nr, nc = r+dr[d], c+dc[d]
-        if section[nr][nc] == 0 and cleaned[nr][nc] == 0:
-            r, c = nr, nc
-
-    else:
-        nr, nc = r+dr[(d+2)%4], c+dc[(d+2)%4]
-        if 0<=nr<N and 0<=nc<M:
-            if section[nr][nc]==0:
-                r, c = nr, nc
-            elif section[nr][nc]==1:
-                canMove = False
+    # [2] 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸(0) 확인
+    if not isCleaned(r,c):
+        nr,nc = r-dr[d],c-dc[d]
+        if 0<=nr<N and 0<=nc<M and status[nr][nc] != 1:
+            r,c = nr,nc
         else:
             canMove = False
+    else: #[3]
+        d = (d-1)%4
+        nr,nc = r+dr[d], c+dc[d]
+        if 0<=nr<N and 0<=nc<M and (nr,nc) not in visited and status[nr][nc] == 0:
+            r,c = nr,nc
 
 
 print(cnt)
