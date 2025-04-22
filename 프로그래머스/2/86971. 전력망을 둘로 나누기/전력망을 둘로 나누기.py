8a7589f44644629ee8bac804591acc870f8a2cf4
WooSung Shin <sws326666@gmail.com>
2025-04-22 11:41:33 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 142.32 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,34 +1,31 @@
 from collections import deque
 
-def solution(n, wires):
-    answer = 101
-    G = [[0]*(n+1) for _ in range(n+1)]
-    for wire in wires:
-        w1,w2 = wire
-        G[w1][w2]=1
-        G[w2][w1]=1
+def bfs(idx,wires):
+    v1,v2 = wires[idx]
     
-    for i in range(len(wires)): #i번째 전력망을 끊기
-        r1,r2 = wires[i]
-        G[r1][r2]=0
-        G[r2][r1]=0
-        
-        sub_G = deque()
-        sub_G.append(r1) #r1을 root로 하는 그래프의 개수
-        visited = set()
-        visited.add(r1)
-        while sub_G:
-            cur = sub_G.popleft()
-            for j in range(1,n+1):
-                if G[cur][j]==1 and j not in visited:
-                    sub_G.append(j)
-                    visited.add(j)
-        
-        G[r1][r2]=1
-        G[r2][r1]=1
-        l1 = len(visited)
-        l2 = n-l1
-        cnt = abs(l1-l2)
-        
-        answer = min(answer,cnt)
+    q = deque()
+    visited = [0]*len(wires)
+    
+    q.append(v1)
+    visited[idx] = 1
+    
+    while q:
+        parent = q.popleft()
+        for i in range(len(wires)):
+            if parent in wires[i] and not visited[i]:
+                q.append(wires[i][0])
+                q.append(wires[i][1])
+                visited[i]=1
+    
+    return sum(visited)
+
+def solution(n, wires):
+    answer = n
+    
+    for idx in range(n-1):
+        g1_cnt = bfs(idx,wires)
+        g2_cnt = n-g1_cnt
+        tmp = abs(g1_cnt-g2_cnt)
+        if tmp<answer:
+            answer = tmp
     return answer
