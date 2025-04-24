9989b44b86582958d20715f55d1316fc1b28ccb1
WooSung Shin <sws326666@gmail.com>
2025-04-24 13:03:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.93 ms, Memory: 9.51 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,24 @@
 from collections import deque
 
 def solution(maps):
-    row,col=len(maps), len(maps[0])
-    mn_d=row*col
-    si,sj=0,0
-    ei,ej=row-1,col-1
-    answer=[-1]
-    q=deque()
-    q.append((si,sj,1))
-    visited=set()
-    visited.add((si,sj))
-    while q:
-        ci,cj,d=q.popleft()
-        if (ci,cj)==(ei,ej):
-            answer.append(d)
-        for di,dj in ((0,1),(1,0),(-1,0),(0,-1)):
-            ni,nj=ci+di,cj+dj                
-            if 0<=ni<row and 0<=nj<col and maps[ni][nj]==1 and (ni,nj) not in visited:
-                q.append((ni,nj,d+1))
-                visited.add((ni,nj))
+    answer = 0
+    n,m = len(maps),len(maps[0])
+    si,sj,ei,ej = 0,0,n-1,m-1
     
-    if len(answer)==1:
-        return -1
-    answer.sort()
-    return answer[1]
+    q = deque()
+    v = [[0]*m for _ in range(n)]
+    
+    q.append((si,sj,1))
+    v[si][sj]=1
+    
+    while q:
+        ci,cj,dist = q.popleft()
+        if (ci,cj)==(ei,ej):
+            return dist
+        for di,dj in ((0,1),(0,-1),(1,0),(-1,0)):
+            ni,nj = ci+di,cj+dj
+            if 0<=ni<n and 0<=nj<m and not v[ni][nj] and maps[ni][nj]==1:
+                q.append((ni,nj,dist+1))
+                v[ni][nj] = 1
+    
+    return -1
