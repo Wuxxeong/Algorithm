376f5bedac6fcc949d2e82fd78b7ead1754170c0
WooSung Shin <sws326666@gmail.com>
2025-08-21 10:51:07 +0900

    [level 3] Title: 네트워크, Time: 0.83 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 4.16 MB, 시간: 0.48 ms
+메모리: 9.31 MB, 시간: 0.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 24일 10:02:53
+2025년 08월 21일 10:51:05
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,20 +1,21 @@
 from collections import deque
+
 def solution(n, computers):
-    graph = 0
-    visited = [False for _ in range(n)]
-    queue = deque()
-    
-    for start_node in range(n):
-        if visited[start_node] == False:
-            queue.append(start_node)
-            visited[start_node] = True
-            graph += 1
-
-        while queue:
-            row = queue.popleft()
-            for col in range(0, n):
-                if computers[row][col] == 1  and not visited[col]:
-                    queue.append(col)
-                    visited[col] = True
-    return graph
-
+    answer = 0
+    v = []
+    for i in range(n): #i와 연결되어 있는 것들
+        if i in v: continue
+        
+        v.append(i)
+        q = deque()
+        q.append(i)
+        while q:
+            cur = q.popleft()
+            computer = computers[cur]
+            for idx in range(n):
+                if computer[idx]==1 and idx not in v:
+                    q.append(idx)
+                    v.append(idx)
+        answer += 1
+        
+    return answer
