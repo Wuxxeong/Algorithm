e3e557f98766759fd1bf1e322a4e953a713d261e
WooSung Shin <sws326666@gmail.com>
2025-09-23 15:58:14 +0900

    [level 2] Title: 타겟 넘버, Time: 1163.63 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,17 +2,16 @@ from collections import deque
 
 def solution(numbers, target):
     answer = 0
-    
     q = deque()
-    q.append((0,0))
+    q.append((0,-1))
     
     while q:
-        v,i = q.popleft()
-        if i==len(numbers):
-            if v == target:
+        cur,i = q.popleft()
+        if i==len(numbers)-1:
+            if cur==target:
                 answer+=1
         else:
-            q.append((v+numbers[i],i+1))
-            q.append((v-numbers[i],i+1))
+            q.append((cur+numbers[i+1],i+1))
+            q.append((cur-numbers[i+1],i+1))
         
     return answer
