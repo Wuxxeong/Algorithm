ca39a5656aefecad8b2906c22298902538878b33
WooSung Shin <sws326666@gmail.com>
2025-09-26 15:38:49 +0900

    [level 2] Title: 카펫, Time: 40.57 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,16 +1,14 @@
-import math
 def solution(brown, yellow):
-    answer = []
-    n = brown+yellow
+    combi = []
+    total = brown+yellow
     
-    candidate = []
-    for i in range(2, int(n**0.5)+1):
-        if n%i==0:
-            candidate.append([n//i,i])
+    # 1) 약수의 조합을 구한다. (N x M)
+    for i in range(1,total//2):
+        if total%i==0:
+            combi.append((total//i, i))
     
-    for row,col in candidate:
-        new_brown = (row+col)*2-4
-        if brown==new_brown:
-            answer=[row,col]
-            break
-    return answer
+    # 2) 2*N + 2*M -4 = total 만족하는지 확인한다.
+    for N,M in combi:
+        if 2*N + 2*M -4 == brown:
+            return [N,M]
+    return -1
