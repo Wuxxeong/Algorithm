e3b3fe89ea55fe2b56a31203eb9cb000f37f4332
WooSung Shin <sws326666@gmail.com>
2025-09-26 15:56:11 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.85 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,24 +1,24 @@
 from collections import deque
 
 def solution(maps):
-    answer = 0
-    n,m = len(maps),len(maps[0])
-    si,sj,ei,ej = 0,0,n-1,m-1
+    answer = 1
+    N,M = len(maps),len(maps[0])
+    si,sj,ei,ej = 0,0,N-1,M-1
     
+    vset = set()
+    vset.add((si,sj))
+
     q = deque()
-    v = [[0]*m for _ in range(n)]
-    
-    q.append((si,sj,1))
-    v[si][sj]=1
+    q.append((si,sj,answer))
     
     while q:
         ci,cj,dist = q.popleft()
         if (ci,cj)==(ei,ej):
             return dist
         for di,dj in ((0,1),(0,-1),(1,0),(-1,0)):
-            ni,nj = ci+di,cj+dj
-            if 0<=ni<n and 0<=nj<m and not v[ni][nj] and maps[ni][nj]==1:
+            ni,nj = ci+di, cj+dj
+            if 0<=ni<N and 0<=nj<M and maps[ni][nj]==1 and (ni,nj) not in vset:
+                vset.add((ni,nj))
                 q.append((ni,nj,dist+1))
-                v[ni][nj] = 1
-    
+                
     return -1
