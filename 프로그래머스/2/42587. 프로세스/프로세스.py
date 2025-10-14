da78720a377f51a99f0f3d59ce682215a676e713
WooSung Shin <sws326666@gmail.com>
2025-10-14 17:31:03 +0900

    [level 2] Title: 프로세스, Time: 0.68 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,20 +1,28 @@
 from collections import deque
+from collections import defaultdict
+
 def solution(priorities, location):
-    result = [] #result[location]에 있는 수 -> priorities에서 index+1
-    pq = deque()
-    for idx,priority in enumerate(priorities):
-        pq.append((idx,priority))
+    answer = 1
+    result = []
+    d = defaultdict(int)
     
-    priorities.sort(reverse=True)
-    mx_i=0
-    while pq:
-        #find max index
-        ci,cp=pq.popleft()
-        if cp==priorities[mx_i]:
-            result.append(ci)
-            mx_i+=1
-        else:
-            pq.append((ci,cp))
-        if mx_i==len(priorities):break
-    #priorities[location]이 몇번째로 실행되는지
-    return result.index(location)+1
+    for idx,p in enumerate(priorities):
+        d[idx] = p
+        
+    
+    q = deque(range(len(priorities)))
+    while q:
+        cur = q.popleft()
+        isAny = False
+        for sub in list(q):
+            if d[sub] > d[cur]:
+                q.append(cur)
+                isAny = True
+                break
+        if not isAny:
+            result.append(cur)
+            if cur == location:
+                return answer
+            answer+=1
+            
+    return answer
