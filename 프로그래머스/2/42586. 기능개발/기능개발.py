1bc0b5f0bdfae0bf632a791d7341478a9c300b9d
WooSung Shin <sws326666@gmail.com>
2025-10-14 20:03:52 +0900

    [level 2] Title: 기능개발, Time: 0.51 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,21 +1,25 @@
-import math
 def solution(progresses, speeds):
     answer = []
-    days = [math.ceil((100-progresses[i])/speeds[i]) for i in range(len(progresses))]
+    day = 0
     
-    count=0
-    release = days[0]
-    for day in days:
-        if day <= release:
-            count += 1
-            continue
-        answer.append(count)
-        release = day
-        count = 1
-    answer.append(count)
+    while progresses:
+        L = len(progresses)
+        #날짜 지남
+        day += 1
+        
+        #진도율 업데이트
+        for i in range(L):
+            progresses[i] += speeds[i]
+    
+        #0번째 prgress 확인
+        cnt = 0
+        while progresses and progresses[0]>=100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt+=1
+            
+        #배포가 되었다면
+        if cnt!=0:
+            answer.append(cnt)
+        
     return answer
-
-'''
-7, 3, 9
-5, 10, 1, 1, 20, 1
-'''
