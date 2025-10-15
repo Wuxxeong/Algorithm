b485caa731e2006db298a6cdb3a1c568070a2988
WooSung Shin <sws326666@gmail.com>
2025-10-15 13:26:52 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.80 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,24 +1,37 @@
+'''
+큐 = 작업 번호, 요청 시각, 소요 시간
+하드디스크 작업 X and 대기 큐 존재 -> 우선순위가 높은 작업 꺼내서 실행
+    우선순위 = 소요시간 작, 요청시각 작, 작업 번호 작 순으로 높음
+하나의 작업만 실행
+요청이 들어오면 작업을 대기 큐에 저장
+
+모든 작업의 반환 시간을 알아내어 이들의 평균을 출력
+
+-> 매번 작업이 추가될 때마다 sort를 할 수 없으니 heapify로 풀자!
+'''
+
 import heapq
 
 def solution(jobs):
-    answer = 0
-    now = 0 #현재시간
-    i = 0   #처리개수
-    start = -1 #마지막 완료시간
-    heap = []
+    answer,time,i = 0,0,0
+    jobs.sort()
+    L = len(jobs)
     
-    while i < len(jobs):
-        for job in jobs:
-            if start < job[0] <= now:
-                heapq.heappush(heap,[job[1],job[0]])
+    waiting = []
+    heapq.heapify(waiting)
+    
+    while i<L or waiting:
+        #[1] 현재 time에 작업을 시행할 수 있는지 process들을 waiting 
+        while jobs and jobs[0][0]<=time:
+            start,total = jobs.pop(0)
+            heapq.heappush(waiting,[total,start,i])
+            i+=1
         
-        if heap:
-            current = heapq.heappop(heap)
-            start = now
-            now += current[0]
-            answer += now - current[1] 
-            i += 1
+        if waiting:
+            ct,cs,ci = heapq.heappop(waiting)
+            time += ct
+            answer += time-cs            
         else:
-            now += 1
-            
-    return answer // len(jobs)
+            time += 1
+
+    return answer//L
