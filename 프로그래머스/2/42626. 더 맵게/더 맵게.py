0bc749d1cf5f107870bb7c62cb2346422fe0bb69
WooSung Shin <sws326666@gmail.com>
2025-10-15 11:46:00 +0900

    [level 2] Title: 더 맵게, Time: 1743.24 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,19 +1,17 @@
 import heapq
+
 def solution(scoville, K):
     answer = 0
-    mixed = 0
     
     heapq.heapify(scoville)
-    while len(scoville) > 0: 
-        if scoville[0] >= K: break
+    
+    while len(scoville)>1: ### -> 두번 pop하니까
+        if scoville[0]>=K: # 가장 작은 원소가 K보다 큰가?
+            return answer
         
-        if len(scoville) >= 2:
-            a = heapq.heappop(scoville)
-            b = heapq.heappop(scoville)
-            mixed = a + b*2
-            answer += 1
-            heapq.heappush(scoville, mixed)
-        elif len(scoville) == 1:
-            heapq.heappop(scoville)
-            answer = -1
-    return answer
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        heapq.heappush(scoville, a+2*b)
+        answer += 1
+        
+    return answer if scoville[0]>=K else -1
