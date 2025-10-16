b200a65dcc11b532e4c388464d17126c61d6b2c2
WooSung Shin <sws326666@gmail.com>
2025-10-16 15:59:48 +0900

    [level 3] Title: 등굣길, Time: 4.57 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,10 +1,13 @@
 def solution(m, n, puddles):
-    answer = 0
-    dp = [[0]*(n+1) for _ in range(m+1)]
-    dp[1][1]=1
-    for i in range(1,m+1):
-        for j in range(1,n+1):
+    dp = [[0]*(m+1) for _ in range(n+1)]
+    #base
+    dp[1][1] = 1
+    
+    for i in range(1,n+1):
+        for j in range(1,m+1):
             if [i,j]==[1,1]: continue
-            if [i,j] in puddles: continue
-            dp[i][j] = dp[i-1][j]+dp[i][j-1]
+            if [j,i] in puddles:
+                dp[i][j]=0
+                continue
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
     return dp[-1][-1]%1000000007
