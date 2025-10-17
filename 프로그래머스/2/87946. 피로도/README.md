075fa05dca0e32336f7758d7916c3da98ad67c33
WooSung Shin <sws326666@gmail.com>
2025-10-17 14:35:33 +0900

    [level 2] Title: 피로도, Time: 48.11 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 16.7 MB, 시간: 79.17 ms
+메모리: 13.8 MB, 시간: 48.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 26일 15:09:05
+2025년 10월 17일 14:35:30
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,22 +1,18 @@
-from collections import deque
-
+'''
+최소 필요 피로도, 소모 피로도
+''' 
+from itertools import permutations
 def solution(k, dungeons):
     answer = -1
-    n = len(dungeons)
-    
-    q = deque()
-    q.append((k, [False]*n, []))
-    
-    while q:
-        ck, used, path = q.popleft()
-        isChange = False
-        for i in range(n):
-            if used[i]==False and ck>=dungeons[i][0]:
-                new_used = used[:]
-                new_used[i]=True
-                q.append((ck-dungeons[i][1], new_used, path+[i]))
-                isChange = True
-        if not isChange:
-            answer = max(answer,sum(used))
+    L = len(dungeons)
+
+    for lst in list(permutations(dungeons,L)):
+        t = k
+        cnt = 0
+        for mn,use in lst:
+            if t>=mn:
+                t-=use
+                cnt+=1
+        answer = max(answer,cnt)
         
     return answer
