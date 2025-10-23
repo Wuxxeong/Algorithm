9374a5668f017fe5a9b3af575925f4a6ab07b5e9
WooSung Shin <sws326666@gmail.com>
2025-10-23 11:04:50 +0900

    [level 4] Title: 징검다리, Time: 143.74 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,25 +1,24 @@
 def solution(distance, rocks, n):
     answer = 0
+    
     rocks.append(distance)
     rocks.sort()
-    left , right = 1, distance
+    left, right = 1,distance
     
-    while left <= right:
-        mid = (left+right)//2   #가정한 거리의 최솟값
+    while left<=right:
+        mid = (left+right)//2
         
         breakRock = 0
         previous = 0
         for rock in rocks:
-            if rock - previous < mid:
-                breakRock += 1
-                if breakRock > n: break
+            if rock-previous<mid:
+                breakRock+=1
+                if breakRock>n:break
             else:
                 previous = rock
-        
-        if breakRock > n:
+        if breakRock>n:
             right = mid-1
         else:
-            answer = mid
             left = mid+1
-                
+            answer = mid
     return answer
