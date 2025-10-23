cf4336b7bb80b2b80e33f14077a0c304ab84a0f5
WooSung Shin <sws326666@gmail.com>
2025-10-23 10:25:12 +0900

    [level 3] Title: 입국심사, Time: 457.30 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,20 +1,23 @@
+'''
+관점을 바꾸어서, 각 심사대가 어떤 시간(mid)동안 총 몇명의 인원을 심사할 수 있는지?
+'''
+
 def solution(n, times):
+    answer = 0
     left = 1
-    right = max(times) * n
+    right = max(times)*n
     
-    while left <= right:
+    while left<=right:
         mid = (left+right)//2
-        people = 0
-     	
+        people = 0 #심사할 수 있는 인원 수
+        
         for time in times:
             people += mid//time
-            if people >= n:
+            if people>=n:
                 break
-        if people >= n:
-            right = mid -1
-        else :
-            left = mid + 1
-    
+        if people>=n:
+            right = mid-1
+        else:
+            left = mid+1
     answer = left
     return answer
-    
